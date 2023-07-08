641b105b70474c23fdadd5686d70c5ec0dba293c
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2023-07-08 18:58:35 +0200

    Added CRUD

--- IsDomasna/Controllers/TicketController.cs
+++ IsDomasna/Controllers/TicketController.cs
@@ -15,7 +15,7 @@ namespace YourAppName.Controllers
             _context = context;
         }
 
-        // GET: Ticket
+        // GET: /
         public IActionResult Index(DateTime? validityDate)
         {
             var tickets = _context.Tickets.AsQueryable();
@@ -27,6 +27,96 @@ namespace YourAppName.Controllers
             }
 
             return View(tickets.ToList());
+        }
+
+        // GET: /Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tickets.Add(ticket);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ticket);
+        }
+
+        // GET: /Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = _context.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // POST: /Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Ticket ticket)
+        {
+            if (id != ticket.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Tickets.Update(ticket);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ticket);
+        }
+
+        // GET: /Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = _context.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // POST: /Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var ticket = _context.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tickets.Remove(ticket);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
